e77d9026ede0f4f1b02e588b9dc66b99f14b2060
Ayo Adedeji <67317859+ayoisio@users.noreply.github.com>
2024-02-19 08:50:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from data_collection import DataCollection
+from src.data_collection import DataCollection
 
 # Stream and collect data for 30 minutes
 data_collection = DataCollection(session_duration=30)
