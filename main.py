9c9f9d23cb46731b9c5f9f99e7b6b37e72bc6700
Ayo Adedeji <67317859+ayoisio@users.noreply.github.com>
2024-02-19 15:12:24 -0800

    Adding buffer arg

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import argparse
 from src.data_collection import DataCollection
 
-# Stream and collect data for 30 minutes
-data_collection = DataCollection(session_duration=30)
+# Create the parser
+parser = argparse.ArgumentParser(description='Collect data from Neurosity device.')
+
+# Add an argument for session duration
+parser.add_argument('--duration', type=int, default=30, help='Duration of the data collection session in minutes.')
+
+parser.add_argument('--buffer', type=int, default=10, help='Duration of the data collection buffer in minutes.')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Stream and collect data for the given duration
+data_collection = DataCollection(session_duration=args.duration, session_buffer=args.buffer)
